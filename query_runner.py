48d43175d72c144bf7bd9bdebd28403f40efc306
saponee <franco.giancola2001@gmail.com>
2025-06-26 18:57:10 +0200

    Aggiorna le query di benchmark e modifica il numero di ripetizioni per ogni query

--- query_runner.py
+++ query_runner.py
@@ -8,31 +8,43 @@ from graphs_query import analyze_and_plot_results_query
 load_dotenv()
 
 # Numero di ripetizioni per ogni query
-REPEAT_PER_QUERY = 3
+REPEAT_PER_QUERY = 1
 
 # Definizione delle query Flux di benchmark
 
 bucket = os.getenv("INFLUX_BUCKET")
 
 QUERIES_FLUX = {
-    "mean_temperature": f'''
+    "aggregation_query":f'''
+      from(bucket: "{bucket}")
+    |> range(start: -14d)
+    |> filter(fn: (r) => r["_measurement"] == "sensor_data")
+    |> aggregateWindow(every: 1h, fn: count, createEmpty: false)
+    |> group(columns: ["device"])
+    |> rename(columns: {{_value: "records_per_hour", _time: "device_per_hour"}})
+    |> sort(columns: ["device"])
+
+    ''',
+    "mean_humidity": f'''
         from(bucket: "{bucket}")
-          |> range(start: -30d)
+          |> range(start: -14d)
           |> filter(fn: (r) => r["_measurement"] == "sensor_data")
-          |> filter(fn: (r) => r["_field"] == "temperature")
+          |> filter(fn: (r) => r["_field"] == "humidity")
           |> group(columns: ["device"])
           |> mean()
     ''',
-    "count_all_records_for_each_device": f'''
+    "count_records_for_each_device": f'''
         from(bucket: "{bucket}")
-          |> range(start: -30d)
+          |> range(start: -14d)
           |> filter(fn: (r) => r["_measurement"] == "sensor_data")
+          |> group(columns: ["device"])
           |> count()
+          |> rename(columns : {{_value:"devices_counted"}})
     ''',
     "days_of_max_temperature":
     f'''
-timeRange = -30d
-maxTemp = from(bucket: "{bucket}")
+    timeRange = -14d
+    maxTemp = from(bucket: "{bucket}")
   |> range(start: timeRange)
   |> filter(fn: (r) =>
     r._measurement == "sensor_data" and
@@ -42,7 +54,7 @@ maxTemp = from(bucket: "{bucket}")
   |> max(column: "_value")
   |> rename(columns:{{_value: "max_temp"}})
 
-data = from(bucket: "{bucket}")
+temperature_data = from(bucket: "{bucket}")
   |> range(start: timeRange)
   |> filter(fn: (r) =>
     r._measurement == "sensor_data" and
@@ -50,7 +62,7 @@ data = from(bucket: "{bucket}")
   )
 
 table_joined = join(
-  tables: {{data: data, max: maxTemp}},
+  tables: {{temperature_data: temperature_data, max: maxTemp}},
   on: ["device"]
 )
 result = table_joined
@@ -65,18 +77,27 @@ result
 }
 
 QUERIES_TS = {
-    "mean_temperature":
+    "aggregation_query":'''
+    SELECT time_bucket('1 hour', time) AS device_per_hour,
+       device,
+       COUNT(*) AS records_per_hour
+        FROM sensors
+        WHERE time >= NOW() - INTERVAL '14 days'
+        GROUP BY device_per_hour, device
+        ORDER BY device_per_hour, device;''',
+
+    "mean_humidity":
     '''
     SELECT device,
-        AVG(temperature) AS mean_temperature
+        AVG(humidity) AS mean_humidity
     FROM sensors
-    WHERE time >= NOW() - INTERVAL '30 days'
+    WHERE time >= NOW() - INTERVAL '14 days'
     GROUP BY device;
     ''',
 
-    "count_all_records_for_each_device": 
+    "count_records_for_each_device": 
     '''
-    SELECT device, COUNT(*)
+    SELECT device, COUNT(*) as devices_counted
     FROM sensors
     GROUP BY device;
     '''
@@ -88,7 +109,7 @@ QUERIES_TS = {
     device,
     MAX(temperature) AS max_temp
   FROM sensors
-  WHERE time >= NOW() - INTERVAL '30 days'
+  WHERE time >= NOW() - INTERVAL '14 days'
   GROUP BY device
 )
 
@@ -97,7 +118,7 @@ FROM sensors s
 JOIN max_values mv
   ON s.device = mv.device
 WHERE 
-  s.time >= NOW() - INTERVAL '30 days' AND
+  s.time >= NOW() - INTERVAL '14 days' AND
   s.temperature = mv.max_temp;
     '''
 }
@@ -108,22 +129,18 @@ def main():
         print(" File 'query_results.csv' precedente rimosso.")
 
     for name, flux_query in QUERIES_FLUX.items():
-        print(f"\n Avvio test di benchmark per la query --> '{name}' (ripetuto {REPEAT_PER_QUERY} volte)")
+        print(f"\n Avvio test di benchmark per la query --> '{name}'")
 
-        for i in range(REPEAT_PER_QUERY):
-            print(f" Esecuzione {i+1} di {REPEAT_PER_QUERY} per la query '{name}'")
-        
+        for i in range(REPEAT_PER_QUERY):        
             run_query_influx(flux_query, name)
     
     for name_ts, ts_query in QUERIES_TS.items() :
-        print(f"\n Avvio test di benchmark per la query sql --> '{name_ts}' (ripetuto {REPEAT_PER_QUERY} volte)")
+        print(f"\n Avvio test di benchmark per la query sql --> '{name_ts}'")
         for i in range(REPEAT_PER_QUERY):
-            print(f" Esecuzione {i+1} di {REPEAT_PER_QUERY} per la query '{name}'")
-
             run_query_timescale(ts_query, name_ts)
 
 
-    print("\nTutti i benchmark riguardanti le query sono stati completati.")
+    print("\nTutti i test riguardanti le query sono stati completati.")
 
 
 if __name__ == "__main__":
